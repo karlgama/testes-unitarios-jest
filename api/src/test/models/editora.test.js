5380ae667d1ed53453a90e55e69dc2dcbd419467
kaique <kaiquesilva49@hotmail.com>
2022-12-13 08:58:24 -0300

    finish

--- api/src/test/models/editora.test.js
+++ api/src/test/models/editora.test.js
@@ -1,3 +1,4 @@
+import { describe, expect, it, jest } from '@jest/globals';
 import Editora from '../../models/editora';
 
 describe('Testando o modelo Editora',()=>{
--- api/src/test/routes/editorasRoutes.test.js
+++ api/src/test/routes/editorasRoutes.test.js
@@ -1,5 +1,8 @@
 import app from '../../app';
 import request from 'supertest';
+import { describe, expect, it,jest } from '@jest/globals';
+
+
 let server;
 
 beforeEach(()=>{
@@ -12,12 +15,12 @@ afterEach(() => {
 });
 
 describe('Get em /editoras',()=>{
-    it('Deve retornar uma lsita de editoras',async()=>{
+    it('Deve retornar uma lista de editoras',async()=>{
         const res = await request(app)
             .get('/editoras')
             .expect(200);
     
-        expect(res.body[0].email).toEqual('e@e.com');
+        expect(res.body[0].email).toEqual('m@m.com');
         });
 });
 
@@ -34,8 +37,32 @@ describe('Post em /editores',()=>{
             })
             .expect(201);        
         idResposta=res.body.content.id;
-    });    
+    });
+    it('deve não adicionar nada oa passaro body vazio',async()=>{
+        await request(app)
+        .post('/editoras')
+        .send({})
+        .expect(400);
+    })    
 });
+
+describe('Put em /editoras/id',()=>{
+    test.each([
+        {nome:'casa do codigo'},
+        {cidade:'sp'},
+        {email:'cdc@cdc.com'},
+    ])
+    ('deve alterar o campo nome',async (param)=>{
+        const req = {request};
+        const spy = jest.spyOn(req,'request');
+        
+        await req.request(app)
+            .put(`/editoras/${idResposta}`)
+            .send(param)
+            .expect(204);
+        expect(spy).toHaveBeenCalled();
+    })
+})
 
 describe('Delete em /editoras',()=>{
     it('deletar o recurso adicionado',async()=>{
@@ -43,4 +70,5 @@ describe('Delete em /editoras',()=>{
         .delete(`/editoras/${idResposta}`)
         .expect(200);
     })
-});    
+});
+
